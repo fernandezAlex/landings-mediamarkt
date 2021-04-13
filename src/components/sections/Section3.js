287645c31796cf56a3b981a6d703da206fb7762c
Marc Boreu <boreu@mediamarkt.es>
2021-04-13 18:13:35 +0200

    fix(section3)/update text

--- src/components/sections/Section3.js
+++ src/components/sections/Section3.js
@@ -9,12 +9,12 @@ const Section3 = ({width, data}) => {
         width < 843 
         ? (
             <div>
-            <h1 className="title">Soluciones a medida para escuelas, centros de formación y universidades</h1>
+            <h1 className="title">Soluciones a medida para tu comercio</h1>
             <SliderSmall data={data}/>
             </div>
         ) : (
             <div>
-            <h1 className="title">Soluciones a medida para escuelas, centros de formación y universidades</h1>
+            <h1 className="title">Soluciones a medida para tu comercio</h1>
             <CardGrid data={data} />
             </div>
         )
