102911f2683ee12aba1ab0b77bbfa6367d615d8f
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-04-20 10:07:06 +0200

    order selector stores

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { conditionalExpression } from "@babel/types";
+
 
 const Select = ({
 	type,
@@ -13,16 +14,41 @@ const Select = ({
 	id,
     data,
     labelDefault,
-
 }) => {
-	var stores = Object.values(data);
-	stores.map(({displayName}, i)=> {
-		var filters=['Próxima','Pinto'];
+
+
+	// Convert in Objects without be a collection
+	var stores = Object.values(data)
+	// Convert Objects in Array
+	var storesArray = [].concat(stores);
+	// Order Array
+	const storesArraySorted = [...storesArray].sort((a, b) => a['displayName'].localeCompare(b['displayName']));
+	// filter Arraya
+	storesArraySorted.map(({displayName}, i)=> {
+		var filters=['Pinto'];
 		if(filters.some(el => displayName.includes(el))) delete stores[i]
 	})
-	
+
+	// With Dynamic Filter to Order (Filter with Select, see down)
+
+	// const [nombre, setNombre] = useState([]);
+	// const sortArray = type => {
+	// 	const types = {
+	// 	  names: 'displayName',
+	// 	  storeID: 'displayName'
+	// 	};
+	// 	const sortProperty = types[type];
+	// 	const sorted = [...storesArraySort].sort((a, b) => a[sortProperty].localeCompare(b[sortProperty]));
+	// 	console.log(sorted);
+	// 	setNombre(sorted);
+	//   };
+
 	return (
 		<div className="container__input">
+			{/* <select  onChange={(e) => sortArray(e.target.value)}>
+				<option value="names">nombre</option>
+				<option value="storeID">id</option>
+			</select> */}
 			<select
 				type={type}
 				name={name}
@@ -35,7 +61,9 @@ const Select = ({
 			>
 				<option selected value="">{labelDefault}</option>
 				{
-					stores.map(({storeId, displayName}, i) => <option key={i} value={storeId}>{displayName}</option>)
+					storesArraySorted.map(({storeId, displayName}, i) => 
+					// storesArraySort.sort((a, b) => a.displayName > b.displayName )
+					<option key={i} value={storeId}>{displayName}</option>)
 				}    
             </select><span className="span__required">*</span>
 			<div className="input__error">
