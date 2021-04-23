70f119e2116147224dcc037811cf07e0dd076814
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-04-23 13:48:26 +0200

    edit sectionImg

--- src/components/sectionimg/SectionImg.js
+++ src/components/sectionimg/SectionImg.js
@@ -2,15 +2,11 @@ import React from "react";
 
  const SectionImg = ({title, img, content, imgPosition}) => {
     return (
-        <div class={`section__img ${imgPosition} ? "" : "imgRight" }`}>
+        <div class={`section__img ${imgPosition ? "" : "imgRight" }`}>
                 <img src={img} alt="MM"/>
             <div class="section__text">
-                <h2>
-                    {title}
-                </h2>
-                <p>
-                   {content}
-                </p>
+                <h2 dangerouslySetInnerHTML={{ __html: title }} />
+                <p dangerouslySetInnerHTML={{ __html: content }} />
             </div>
         </div>
     );
--- src/data/cards.js
+++ src/data/cards.js
@@ -1,12 +1,12 @@
 export const cardsSection1 = [
     {
       title: "Una alarma más completa.",
-      content: "Asistencia inmediata: ante una incidencia o intrusión,nuestra Central Receptora de Alarmas dará respuesta enmenos de 29 segundos.",
+      content: "Asistencia inmediata: ante una incidencia o intrusión,nuestra Central Receptora de Alarmas dará respuesta enmenos de 29 segundos.<br> Aviso a la policía 24horas:nuestros profesionales están preparados para ofrecer ayuda de inmediato y avisar a la policía,24horas al día,365días al año.<br> Protección total:Te protegemos fuera de casa ante situaciones de riesgo con el servicio Contigo ycuidamos de todas tus comunicaciones connuestra seguridad digital.<br><br> Alarma inteligente: si olvidas conectar tu alarma como lo haces habitualmente, te enviaremos una notificación vía App para avisarte.<br><br> Además, con la App podrás conectar y desconectar la alarma, ver lo que pasa en tu hogar en tiempo real y muchas otras cosas más.",
       img: "https://cms-images.mmst.eu/2rj3gcd43pmw/X1s76hIAACQAcE2X/7b1ca0f17647631b0b498811510433c5/Prismic_LP_Visual_Energielabels_1257x703_Teaser_1.jpg?q=80",
       imgPosition: true
     },
     {
-      title: "Y ahora llévate la mejor",
+      title: "Y ahora llévate la mejor conexión al mejor precio,con Conecta Max.",
       content: "Asistencia inmediata: ante una incidencia o intrusión,nuestra Central Receptora de Alarmas dará respuesta enmenos de 29 segundos.",
       img: "https://cms-images.mmst.eu/2rj3gcd43pmw/X1s76hIAACQAcE2X/7b1ca0f17647631b0b498811510433c5/Prismic_LP_Visual_Energielabels_1257x703_Teaser_1.jpg?q=80",
       imgPosition: false
