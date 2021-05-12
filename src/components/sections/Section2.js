ddb6e02e869252465e976dd0d973bbb151e9ba64
Marc Boreu <boreu@mediamarkt.es>
2021-05-12 10:40:11 +0200

    fix(styles)/update styles

--- src/components/sections/Section2.js
+++ src/components/sections/Section2.js
@@ -4,7 +4,7 @@ import CardTextGrid from "../cards/CardTextGrid";
 const Section2 = ({ data }) => {
   return (
       <div className="section">
-      <div>
+      <div className="cardTextGrid__title">
         <p>Nuestra propuesta incluye:</p>
       </div>
       <CardTextGrid data={data}/>
