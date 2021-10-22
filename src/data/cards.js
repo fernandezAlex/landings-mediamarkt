c30380264f03d5be3ba8272cc32c1a303798282f
anperezmm <79531391+anperezmm@users.noreply.github.com>
2021-10-22 11:00:23 +0200

    (feature)/Replace prices by other text. Why? Legal reasons

--- src/data/cards.js
+++ src/data/cards.js
@@ -4,14 +4,12 @@ export const cardsSection = [
       content: `<h2>Pack hostelería</h2>
       <h4>Incluye:</h4>
       <ul><li>Terminal táctil PS3415 con W10 IOT instalado</li><li>Impresora Posiflex PP-8802</li><li>Software de gestión multisectorial No problem gestión</li></ul>
-      <p>Precio 953,95€*</p>
-      <span class="red__mm"><small>*Precio sin impuestos incluidos.</small></span>
+      <span class="red__mm"><small>*No incluye licencia mensual.</small></span>
       <br /><br /><br />
       <h2>Pack comercio</h2>
       <h4>Incluye:</h4>
       <ul><li>Terminal táctil PS3415 con W10 IOT instalado</li><li>Impresora Posiflex PP-8802</li><li>Software de gestión multisectorial No problem gestión</li><li>Lector de código de barras Posiflex CD-3200 + Soporte</li></ul>
-      <p>Precio 1.029,51€*</p>
-      <span class="red__mm"><small>*Precio sin impuestos incluidos.</small></span>
+      <span class="red__mm"><small>*No incluye licencia mensual.</small></span>
       `,
       imgThumbnail:
         "https://specials.mediamarkt.es/assets-react/b2b-hospitality/tpv/tpv_800x600.jpg",
