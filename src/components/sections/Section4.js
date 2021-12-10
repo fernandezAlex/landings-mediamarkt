a9ff293ec40dc69899b2408c72538cafad88eea9
fernandezAlex <alex.fernandez.arroyo@gmail.com>
2021-12-10 11:22:38 +0100

    fix error orthographic

--- src/components/sections/Section4.js
+++ src/components/sections/Section4.js
@@ -10,7 +10,7 @@ const Section4 = ({ width }) => {
             <div className="Cellstyled__StyledCell-sc-1wk5bje-0 bcdKi">
               <div className="StepByStepstyled__StyledContentBlock-aps9mk-1 kXxEBi">
                 <h2 color="white" fontFamily="default" spacing="base" fontSize="xxl"
-                    aria-label="Así es como funciona" className="Typostyled__StyledInfoTypo-sc-1jga2g7-0 eqGOUK">Solicita ahora una oferta individual para cantiddades superiores a cinco productos</h2>
+                    aria-label="Así es como funciona" className="Typostyled__StyledInfoTypo-sc-1jga2g7-0 eqGOUK">Solicita ahora una oferta individual para cantidades superiores a cinco productos</h2>
                 <p color="white" fontSize="sm" className="Typostyled__StyledInfoTypo-sc-1jga2g7-0 FAiMO"></p>
                 <div direction="horizontal" className="Spacer__StyledSpacer-sc-1wx27iz-0 eMgmEz"></div>
                 <div display="flex"
